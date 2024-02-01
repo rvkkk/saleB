c413e1528f034f3b9092a96da97e9ef20f5a4c3f
rvkk2828 <rvkk2828@gmail.com>
2024-02-01 18:42:21 +0200

    add category

--- src/pages/AddCategory.js
+++ src/pages/AddCategory.js
@@ -1,55 +1,10 @@
 import {
-  Box,
   Button,
-  Divider,
-  Flex,
-  FormControl,
-  FormLabel,
-  Grid,
-  IconButton,
-  Image,
-  Spacer,
-  Switch,
-  Text,
-  Checkbox,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  useDisclosure,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  CloseButton,
-  Stack,
-  ChakraProvider,
-  Popover,
-  PopoverTrigger,
-  PopoverHeader,
-  PopoverBody,
-  PopoverArrow,
-  PopoverCloseButton,
-  PopoverContent,
   Input as ChakraInput,
-  GridItem,
 } from "@chakra-ui/react";
 import React from "react";
-import Bbutton from "../components/Button";
-import { DatePicker, TimePicker } from "react-rainbow-components";
-import { format } from "date-fns";
-import { AiFillExclamationCircle, AiOutlinePicture } from "react-icons/ai";
-import Container from "../components/Container";
-import FileUploader from "../components/FileUploader";
-import { RightIcon2, ShareIcon, TrashIcon } from "../components/Icons";
-import Input, { CategoryInput } from "../components/Input";
-import Layout from "../components/Layout";
-import ProductImageSelect from "../components/ProductImageSelect";
-import TextArea from "../components/TextArea";
-import Loader from "../components/Loader";
-import { useState, useEffect } from "react";
-import { addProduct } from "../utils/api/products";
-import { addAuctionProduct } from "../utils/api/auctionProducts";
-import { routes } from "../routes";
+import Input from "../components/Input";
+import { useState } from "react";
 import { addCategory } from "../utils/api/categories";
 
 export default function AddCategory() {
