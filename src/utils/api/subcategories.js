3247d1a935726d1d6626e98dd9e71e72e978575d
rvkk2828 <rvkk2828@gmail.com>
2024-02-09 02:25:06 +0200

    l

--- src/utils/api/subcategories.js
+++ src/utils/api/subcategories.js
@@ -38,7 +38,6 @@ export const addSubcategory = (categoryName, title, name, image) => {
       .post(`${baseURL}subcategory/${categoryName}`, formData, {
         headers: {
            "Content-Type": 'multipart/form-data',
-           "Authorization": `Bearer ${localStorage.getItem("token")}`,
         }})
       .then((res) => {
         resolve(res.data);
